c053d81ac845d66cbefdafdd0dd6d7a8b60ee4e9
anikarevalo <anika.arevalo@gmail.com>
2022-06-28 11:27:17 +0200

    'changed gitignore'

---
+++ app.py
@@ -0,0 +1,40 @@
+
+
+import pandas as pd
+import sys
+import json
+import joblib 
+from predictfile import predict_label
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def alive():
+    return render_template("index.html")    # Template appearance before result is shown
+
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    """
+    This function will be used for the prediction of customer review rating (either positive or negative)
+    """
+    
+    if request.method == 'POST':
+            client_review = request.form.get('review')
+             
+
+            
+            
+            ponev=predict_label(client_review)
+            
+        
+            return render_template("index.html", result = ponev)
+
+    if request.method == 'GET':
+        return render_template("index.html")    # Positive or Negative result shown
+    
+
+if __name__ == '__main__':
+   app.run(debug=True, port=8000)
---
+++ predictfile.py
@@ -0,0 +1,33 @@
+
+
+# Installing machine learning and NLP libraries
+import tensorflow as tf
+
+
+from transformers import DistilBertTokenizerFast
+from transformers import TFDistilBertForSequenceClassification
+
+
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)  # Ignoring warnings
+
+#  A function that predicts label (positive or negative) when user inputs according to the parameters of the TFDistilBertForSequenceClassification model
+def predict_label(client_review):    
+    # Initializing a tokenizer
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
+    # Loading the saved re-trained, pre-trained DistilBert NLP model
+    loaded_model = TFDistilBertForSequenceClassification.from_pretrained('drive/MyDrive/tdb_sentiment')
+
+    # Predicting on unseen/new data
+    predict_input = tokenizer.encode(client_review,
+                                    truncation=True, 
+                                    padding=True, 
+                                    return_tensors='tf')
+    tf_output = loaded_model.predict(predict_input)[0]
+    tf_prediction = tf.nn.softmax(tf_output, axis=1)  # Activation set to softmax for Sparse Categorical Entropy loss
+    labels = ['Negative','Positive']
+    label = tf.argmax(tf_prediction, axis=1)
+    label = label.numpy()
+    
+    # Returning prediction of label (postive or negative)
+    return labels[label[0]]
